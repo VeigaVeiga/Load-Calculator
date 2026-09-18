5d7f0674d458f11df8e6f22d6fb5f42aee52213a
Veiga <50994187+VeigaVeiga@users.noreply.github.com>
2026-09-19 02:57:16 +0800

    V29: replace temporary packing engine edit with working engine

--- src/packing/packer.ts
+++ src/packing/packer.ts
@@ -1,11 +1,11 @@
 import type { Cargo, Container, PlacedCargo } from '../types'
 import { dims, overlap } from './geometry'
 
-const SNAP = 10
-const EPS = 1
+const SNAP=10
+const EPS=1
 
-type Orientation = { length: number; width: number; rotation: 0 | 90 }
-type Unit = { cargo: Cargo; index: number }
-type Point = { x: number; y: number; z: number }
+type Orientation={length:number;width:number;rotation:0|90}
+type Unit={cargo:Cargo;index:number}
+type Point={x:number;y:number;z:number}
 
-actionable
+action
