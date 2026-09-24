7f76bc69c304020b92234162d21961e47b0b5415
Veiga <50994187+VeigaVeiga@users.noreply.github.com>
2026-09-24 17:38:36 +0800

    Remove heavy 3D warehouse layer for 2.5D performance

--- src/components/ContainerFloor.tsx
+++ src/components/ContainerFloor.tsx
@@ -1,7 +1,6 @@
 import { Line } from '@react-three/drei'
 import type { ReactElement } from 'react'
 import type { Container } from '../types'
-import ApertureLogisticsScene from './ApertureLogisticsScene'
 const S=.001
 export default function ContainerFloor({container}:{container:Container}){
  const L=container.length*S,W=container.width*S
@@ -25,7 +24,6 @@ export default function ContainerFloor({container}:{container:Container}){
  }
  addAxisLines('x'); addAxisLines('y')
  return <group>
-  <ApertureLogisticsScene />
   <mesh position={[0,0,-container.floorThickness*S/2]} receiveShadow><boxGeometry args={[L,W,container.floorThickness*S]}/><meshStandardMaterial color="#aeb5b6" roughness={.9} /></mesh>
   {lines}
  </group>
